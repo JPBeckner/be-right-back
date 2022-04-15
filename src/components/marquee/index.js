6f6723a58ed8281ffc60d7139fdae63878036b73
JPBeckner <pedrobeckner@hotmail.com>
2022-04-15 17:33:29 -0300

    feat: create marquee component

---
+++ src/components/marquee/index.js
@@ -0,0 +1,20 @@
+import "./Marquee.css";
+import { t } from "i18next";
+
+function Marquee({backgroundColor, color}) {
+
+  return (
+    <div 
+    className="marquee"
+    style={{ "backgroundColor": backgroundColor, "color": color }}
+    // background-color={backgroundColor}
+    // color={color}
+    >
+        <p>
+          {t("message")}
+        </p>
+    </div>
+  );
+}
+
+export default Marquee;
